4923d10a93fb1473bde7f5c09d17e760d0402e08
Nguyễn Ngọc Minh Quân <32874438+nguyenquan263@users.noreply.github.com>
2020-09-19 00:23:14 -0500

    Finish the basic layout for Calendar Request Page

--- una-digest/src/main.js
+++ una-digest/src/main.js
@@ -3,27 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-
 import './assets/admin-site/vendor/fontawesome-free/css/all.min.css';
 import './assets/admin-site/fonts/font1.css';
 import './assets/admin-site/css/sb-admin-2.min.css';
 import './assets/commons/jquery.dataTables.min.css';
 import './assets/commons/toastr.min.css';
 import './assets/commons/jquery-confirm.min.css';
-// import './assets/for-client/css/agency.min.css';
-// import './assets/for-client/font/font1.css';
-// import './assets/for-client/font/font2.css';
-// import './assets/for-client/font/font3.css';
-// import './assets/for-client/font/font4.css';
 import './assets/commons/summernote.min.css';
+import './assets/commons/JQueryDateTimePicker/lib/themes/classic.css';
+import './assets/commons/JQueryDateTimePicker/lib/themes/classic.date.css';
+import './assets/commons/JQueryDateTimePicker/lib/themes/classic.time.css';
+
+
+
 
 import './assets/commons/jquery-global';
 import './assets/commons/datatable-global';
 import './assets/commons/toastr-global';
 import './assets/commons/summernote-global';
-
 import './assets/admin-site/vendor/bootstrap/js/bootstrap.bundle';
 import './assets/admin-site/vendor/jquery-easing/jquery.easing';
 import './assets/commons/jquery-confirm.min.js';
+import './assets/commons/JQueryDateTimePicker/lib/picker';
+import './assets/commons/JQueryDateTimePicker/lib/picker.date';
+import './assets/commons/JQueryDateTimePicker/lib/picker.time';
+
 
 createApp(App).use(store).use(router).mount('#app')
--- una-digest/src/views/CalendarRequest.vue
+++ una-digest/src/views/CalendarRequest.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="col-md-12">
-    <h1>Request a Calendar</h1>
+<div class="container col-md-12">
 
-    <div class="full" ng-if="calendarReq">
+    <h1>Request a Calendar</h1>
+    <!-- <div class="full" ng-if="calendarReq">
 		<p class="highlight">
 			<strong>
 				Please read the following guidelines VERY carefully before submitting your campus event request:
@@ -25,7 +25,7 @@
 				Note: this is nothing more than a request for a <strong>calendar listing</strong> of your event. To reserve a venue, <a ui-sref="calendar.venue">click here</a>.
 			</li>
 		</ul>
-	</div>
+	</div> -->
 
     <div class="full">
 		<p><em>
@@ -73,12 +73,9 @@
                         Start Date*
                     </label>
                     <!--input class="form-control" type="date" id="Start_Date" ng-model="requestForm.startDate" placeholder="mm/dd/yyyy"/-->
-                    <p class="input-group calendar-datepicker">
-                        <input type="text" name="startDate" class="form-control" datepicker-popup="MM/dd/yyyy" v-model="calendar.period.startDate" is-open="datepicker.startDate.open" min-date="today" close-text="Close" placeholder="mm/dd/yyyy" show-weeks="false" required="required"/>
-                        <span class="input-group-btn">
-                            <button type="button" class="btn btn-default"><i class="fa fa-calendar"></i></button>
-                        </span>
-                    </p>
+                    
+                    <input type="text" id="startDate" class="form-control" v-model="calendar.period.startDate" placeholder="mm/dd/yyyy" required="required"/>
+ 
                 </div>
 
                 <div class="form-group col-md-6">
@@ -86,34 +83,129 @@
                         End Date (optional)
                     </label>
                     <!--input class="form-control" type="date" id="End_Date" ng-model="requestForm.endDate" placeholder="mm/dd/yyyy"/-->
-                    <p class="input-group calendar-datepicker">
-                        <input type="text" name="endDate" class="form-control" datepicker-popup="MM/dd/yyyy" v-model="calendar.period.endDate" is-open="datepicker.endDate.open" min-date="today" close-text="Close" placeholder="mm/dd/yyyy" show-weeks="false"/>
-                    <span class="input-group-btn">
-                        <button type="button" class="btn btn-default" ><i class="fa fa-calendar"></i></button>
+                    
+                    <input type="text" id="endDate" class="form-control" v-model="calendar.period.endDate" placeholder="mm/dd/yyyy"/>
+                </div>
+            </div>
+
+            <div class="form-row ml-2 mr-2">
+                <div class="form-group col-md-6">
+                    <label for="Start_Date">
+                        Start Time (optional)
+                    </label>
+                    <!--input class="form-control" type="date" id="Start_Date" ng-model="requestForm.startDate" placeholder="mm/dd/yyyy"/-->
+                    
+                    <input type="text" id="startTime" class="form-control" v-model="calendar.period.startTime" placeholder="hh:mm"/>
+ 
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="End_Date">
+                        End Time (optional)
+                    </label>
+                    <!--input class="form-control" type="date" id="End_Date" ng-model="requestForm.endDate" placeholder="mm/dd/yyyy"/-->
+                    
+                    <input type="text" id="endTime" class="form-control" v-model="calendar.period.endTime" placeholder="hh:mm"/>
+                </div>
+            </div>
+
+            <div class="form-group col-md-12">
+				<label for="Announcement_Title">
+					Location (optional)
+				</label>
+				<input type="text" class="form-control" id="Announcement_Title" v-model="calendar.location" name="accouncementLocation">
+			</div>
+
+            <div class="form-group col-md-12">
+				<label for="Summary">
+					Calendar Summary*
+				</label>
+				<textarea class="form-control" rows="5" id="summary" >
+				</textarea>
+			</div>
+
+            <div class="form-group col-md-12" >
+				<label for="Link_to_more_info">
+					Link to more information (optional)
+				</label>
+				<input class="form-control" type="text" name="url" id="Link_to_more_info" placeholder="http://" />
+				<span class="help-block">
+					Contact your department's Web Lion about creating a page for your Calendar. If you need assistance linking to a PDF document please contact Jeremy Woods (jdwoods@una.edu) or Mitchel Moon (mrmoon@una.edu).
+				</span>	
+			</div>
+
+			<div class="form-group col-md-12">
+				<label for="Supplemental_image">
+					Supplemental image (optional)
+				</label>
+				<input class="form-control" type="file" name="file" id="image" accept="image/x-png, image/jpeg" />
+
+                <span class="help-block">
+					Acceptable formats: <strong>.jpg, or .png</strong><br>
+					Must be under 3 MB
+				</span>
+			</div>
+
+            <!-- Just for spacing above h3 -->
+			<div class="row">
+				&nbsp;
+			</div>
+
+			<h3 class="page-header">
+				Contact Information
+			</h3>
+            <hr>
+			<div class="form-group col-md-12">
+				<label for="Contact_Name">
+					Contact Name*
+				</label>
+                
+				<input class="form-control" type="text" id="Contact_Name" name="contactName"  required="required"/>
+				<span class="help-block">
+					You must be an authorized representative of your department/organization.
+				</span>
+			</div>
+            <div class="form-row ml-2 mr-2">
+                <div class="form-group col-md-6" >
+                    <label class="control-label" for="Contact_Phone">
+                        Contact Phone*
+                    </label>
+                    <input class="form-control" type="tel" name="contactPhone" placeholder="555-555-5555" id="contactPhone" required="required"/>
+                    <span class="help-block">
+                        What number can people call with questions?
                     </span>
-                </p>
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="Contact_Email">
+                        Contact Email*
+                    </label>
+                    <input class="form-control" type="email" name="contactEmail" id="Contact_Email" placeholder="@una.edu"  required="required"/>
+                    <span class="help-block">
+                        Must use your UNA.edu email address.
+                    </span>
                 </div>
             </div>
             
-            <div class="form-row ml-2 mr-2">
-                <div class="form-group col-md-6" ng-if="showDateTimeFields">
-                    <label for="Start_Time">
-                        Start Time (optional)
-                    </label>
-                    <input type="checkbox" ng-model="useField.startTime" ng-change="resetTime('startTime')"/>
-                    <!--input class="form-control" type="time" id="Start_Time" ng-model="requestForm.startTime" ng-show="useStartTime" /-->
-                    <timepicker class="calendar-timepicker" id="Start_Time" ng-model="requestForm.startTime" hour-step="1" minute-step="5" show-meridian="true" ng-show="useField.startTime"></timepicker>
-                </div>
+            <div class="form-group col-md-12">
+				<label for="Sponsoring_Department_Organization">
+					Sponsoring Department/Organization*
+				</label>
+				<input class="form-control" type="text" id="Sponsoring_Department_Organization" name="sponsorOrg"  required="required"/>
+			</div>
 
-                <div class="form-group col-md-6" ng-if="showDateTimeFields">
-                    <label for="End_Time">
-                        End Time (optional)
-                    </label>
-                    <input type="checkbox" ng-model="useField.endTime" ng-change="resetTime('endTime')"/>
-                    <timepicker class="calendar-timepicker" id="End_Time" ng-model="requestForm.endTime" hour-step="1" minute-step="5" show-meridian="true" ng-show="useField.endTime"></timepicker>
-                    <!--input class="form-control" type="time" id="End_Time" ng-model="requestForm.endTime" ng-show="useEndTime" /-->
-                </div>
-            </div>
+			<br/>
+			<br/>
+
+			<div class="form-group col-md-12" >
+				<button type="button" class="btn btn-primary" >
+					Submit
+				</button>
+				<span class="help-block">
+					Please make sure all required fields have been filled out correctly.
+				</span>
+			</div>
+            
         </form>
 
     </div>
@@ -127,14 +219,43 @@ import CalendarModel from '../models/Calendar';
 export default {
     data: function() {
         return {
-            calendarCategories: ['Student', 'Faculty', 'Staff', 'All Employee', 'Everyone', 'Bereavement'],
-            calendar: null
+            calendarCategories: ['Academics'
+            , 'Departments'
+            , 'Athletics'
+            , 'Club Sports'
+            , 'Registered Student Organizations'
+            , 'Off-Campus Opportunities'
+            , 'Job Opportunities'],
+            calendar: null,
         }
     },
     created: function() {
         this.calendar = new CalendarModel.Calendar();
         this.calendar.period.startDate = "";
         this.calendar.period.endDate = "";
+        this.calendar.period.startTime = "";
+        this.calendar.period.endTime = "";
+    },
+    mounted: function() {
+        $('#startDate').pickadate({
+            format: 'mm/dd/yyyy',
+            min: true
+        });
+
+        $('#endDate').pickadate({
+            format: 'mm/dd/yyyy',
+            min: true
+        });
+
+        $('#startTime').pickatime();
+        $('#endTime').pickatime();
+
+        $('#summary').summernote({
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline']],
+                ['font', ['strikethrough']]
+            ]
+        });
     }
 }
 </script>
